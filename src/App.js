31d6fac988da38013c99a0fe53c2310386780e6d
utkarsh shrivastava <shrivastava.utkarsh0@gmail.com>
2021-05-26 16:44:56 +0530

    CSS change

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+
+  var name = 'Utkarsh'
+  var loggedIn = false
   return (
+    <React.Fragment>
+      
     <div className="App">
       <header className="App-header">
+      <h1>Welcome to react page</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello World !!!!
-         
-         This is my first React Page !!!
-         
+         Hello {loggedIn ? name : 'World'} 
         </p>
       </header>
     </div>
+    </React.Fragment>
   );
 }
 
