7c976ffc48af94650a6b4fef5ea75023ab54cdd1
utkarsh shrivastava <shrivastava.utkarsh0@gmail.com>
2021-05-26 16:55:26 +0530

    image change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import Mountains from './assests/mountainsandtree.svg';
 
 function App() {
 
@@ -11,8 +12,9 @@ function App() {
       
     <div className="App">
       <header className="App-header">
-      <h1>Welcome to react page</h1>
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>Welcome To React Page</h1>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={Mountains} className="App-logo" alt="logo" />
         <p>
          Hello {loggedIn ? name : 'World'} 
         </p>
